cded0f01a4468c32f49df8b0b0e2741daf3f2267
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-03-24 16:25:15 -0300

    feat: add database context with students table

---
+++ Src/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using courses_dotnet_api.Src.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace courses_dotnet_api.Src.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options)
+        : base(options) { }
+
+    public DbSet<Student> Students { get; set; }
+}

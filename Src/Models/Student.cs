aae792c1548b53783e488b083fae75af8fe2418e
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-03-24 16:24:43 -0300

    feat: add student model

---
+++ Src/Models/Student.cs
@@ -0,0 +1,9 @@
+namespace courses_dotnet_api.Src.Models;
+
+public class Student
+{
+    public int Id { get; set; }
+    public required string Rut { get; set; }
+    public required string Name { get; set; }
+    public required string Email { get; set; }
+}

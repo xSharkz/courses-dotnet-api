f27d9a1f88c4b30aeb7628fd68db58b1fc9f89ae
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-03-31 23:16:52 -0300

    feat: update student model

--- Src/Models/Student.cs
+++ Src/Models/Student.cs
@@ -6,4 +6,5 @@ public class Student
     public required string Rut { get; set; }
     public required string Name { get; set; }
     public required string Email { get; set; }
+    public required string Password { get; set; }
 }

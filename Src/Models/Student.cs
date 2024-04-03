81d59c2fdfaca5e2625d61c909f955bea7211c23
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-04-03 15:38:19 -0300

    feat: update student model

--- Src/Models/Student.cs
+++ Src/Models/Student.cs
@@ -6,5 +6,6 @@ public class Student
     public required string Rut { get; set; }
     public required string Name { get; set; }
     public required string Email { get; set; }
-    public required string Password { get; set; }
+    public required byte[] PasswordHash { get; set; }
+    public required byte[] PasswordSalt { get; set; }
 }

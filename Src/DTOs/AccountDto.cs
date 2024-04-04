4865e6fadddc4866c3c0377d9c3e080ce72a9fab
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-04-04 20:45:21 -0300

    feat: delete dtos

--- Src/DTOs/AccountDto.cs
+++
@@ -1,9 +0,0 @@
-namespace courses_dotnet_api.Src.DTOs;
-
-public class AccountDto
-{
-    public required string Rut { get; set; }
-    public required string Name { get; set; }
-    public required string Email { get; set; }
-    public required string Token { get; set; }
-}
--- Src/DTOs/LoginStudentDto.cs
+++
@@ -1,7 +0,0 @@
-namespace courses_dotnet_api.Src.DTOs;
-
-public class LoginStudentDto
-{
-    public required string Email { get; set; }
-    public required string Password { get; set; }
-}

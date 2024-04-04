fe0961c31432c0b28f0d2be0655e79c30e3c86d3
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2024-04-04 20:46:35 -0300

    refactor: delete mappers

--- Src/Helpers/AutoMapperProfiles.cs
+++ Src/Helpers/AutoMapperProfiles.cs
@@ -1,15 +1,8 @@
 using AutoMapper;
-using courses_dotnet_api.Src.DTOs;
-using courses_dotnet_api.Src.Models;
 
 namespace courses_dotnet_api.Src.Helpers;
 
 public class AutoMapperProfiles : Profile
 {
-    public AutoMapperProfiles()
-    {
-        CreateMap<CreateStudentDto, Student>();
-        CreateMap<Student, StudentDto>();
-        CreateMap<UpdateStudentDto, Student>();
-    }
+    public AutoMapperProfiles() { }
 }
